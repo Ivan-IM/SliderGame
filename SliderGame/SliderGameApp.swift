19cad5175b9cc2921cfb57fbc6a8da1b0d37399b
iMarishin <move_up@icloud.com>
2021-08-06 12:59:40 +0500

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Иван Маришин on 06.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
